ef9756ad6d4cc149347cd8013af42b3f176f8cba
Noel2 <151483346+Noel-leoN@users.noreply.github.com>
2026-04-17 23:37:04 +0800

    feat: add ModAssemblyInfo class to expose assembly metadata as constants

--- EconomyEX/ModAssemblyInfo/ModAssemblyInfo.cs
+++ EconomyEX/ModAssemblyInfo/ModAssemblyInfo.cs
@@ -10,7 +10,7 @@ namespace EconomyEX
     public class ModAssemblyInfo
     {
         public const string Name        = "EconomyEX";
-        public const string Version     = "1.1.7";
+        public const string Version     = "1.2.0";
         public const string Title       = "EconomyEX";
         public const string Description = "Economy Extra Fix and Performance Boost";
     }
