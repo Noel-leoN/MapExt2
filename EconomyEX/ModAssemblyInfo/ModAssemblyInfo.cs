97a7a731e672bce4bde178b7b3099917f39c6701
Noel2 <151483346+Noel-leoN@users.noreply.github.com>
2026-05-01 22:15:46 +0800

    feat: add ModAssemblyInfo class for automated version and metadata tracking

--- EconomyEX/ModAssemblyInfo/ModAssemblyInfo.cs
+++ EconomyEX/ModAssemblyInfo/ModAssemblyInfo.cs
@@ -10,7 +10,7 @@ namespace EconomyEX
     public class ModAssemblyInfo
     {
         public const string Name        = "EconomyEX";
-        public const string Version     = "1.2.2";
+        public const string Version     = "1.3.0";
         public const string Title       = "EconomyEX";
         public const string Description = "Economy Extra Fix and Performance Boost";
     }
